bc5491aaa3d8d0108f384e3d0d604118fa9baac2
shisongWjj <sslovewjj891029>
2020-08-10 00:56:31 +0800

    提交

---
+++ service-api/src/main/java/com/ss/spring/cloud/example/App.java
@@ -0,0 +1,13 @@
+package com.ss.spring.cloud.example;
+
+/**
+ * ServiceConsumerApplication
+ *
+ * @author shisong
+ * @date 2020-08-10
+ */
+public class App {
+
+    public static void main(String[] args) {
+    }
+}
---
+++ service-api/src/main/java/com/ss/spring/cloud/example/ISayHelloService.java
@@ -0,0 +1,10 @@
+package com.ss.spring.cloud.example;
+
+/**
+ * ISayHelloSerivice
+ *
+ * @author shisong
+ * @date 2020-08-10
+ */
+public class ISayHelloService {
+}
